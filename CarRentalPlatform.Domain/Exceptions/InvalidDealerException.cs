f91e4a114918dcad81d27dd7b245988c8ca6d376
vikto <v.atanassovv@gmail.com>
2024-05-13 22:17:29 +0300

    add InvalidDealerException

---
+++ CarRentalPlatform.Domain/Exceptions/InvalidDealerException.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalPlatform.Domain.Exceptions
+{
+    public class InvalidDealerException : BaseDomainException
+    {
+        public InvalidDealerException()
+        {
+        }
+
+        public InvalidDealerException(string message) => this.Message = message;
+    }
+}
