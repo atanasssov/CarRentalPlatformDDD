43b33e9d7290d9401c3958ea16eb7ad4d943f450
vikto <v.atanassovv@gmail.com>
2024-05-13 22:16:21 +0300

    add BaseDomainException

---
+++ CarRentalPlatform.Domain/Exceptions/BaseDomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRentalPlatform.Domain.Exceptions
+{
+    public abstract class BaseDomainException : Exception
+    {
+        private string? message;
+
+        public new string Message
+        {
+            get => this.message ?? base.Message;
+            set => this.message = value;
+        }
+    }
+}
