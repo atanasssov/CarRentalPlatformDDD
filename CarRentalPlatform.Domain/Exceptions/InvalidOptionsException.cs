8ad4b4323b7317cf844466ea398757303d6dd4e9
vikto <v.atanassovv@gmail.com>
2024-05-13 22:18:03 +0300

    add InvalidOptionsException

---
+++ CarRentalPlatform.Domain/Exceptions/InvalidOptionsException.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalPlatform.Domain.Exceptions
+{
+    public class InvalidOptionsException : BaseDomainException
+    {
+        public InvalidOptionsException()
+        {
+        }
+
+        public InvalidOptionsException(string message) => this.Message = message;
+    }
+}
