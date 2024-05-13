bfff8c48210e26bd8ba67e7128e8c62bb89cb22d
vikto <v.atanassovv@gmail.com>
2024-05-13 22:18:39 +0300

    add InvalidPhoneNumberException

---
+++ CarRentalPlatform.Domain/Exceptions/InvalidPhoneNumberException.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalPlatform.Domain.Exceptions
+{
+    public class InvalidPhoneNumberException : BaseDomainException
+    {
+        public InvalidPhoneNumberException()
+        {
+        }
+
+        public InvalidPhoneNumberException(string message) => this.Message = message;
+    }
+}
