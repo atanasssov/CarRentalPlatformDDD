92e7efe1866daf9ff33dcd09b44550ae573057d2
vikto <v.atanassovv@gmail.com>
2024-05-14 13:34:47 +0300

    add Manufacturer class

---
+++ CarRentalPlatform.Domain/Models/CarAds/Manufacturer.cs
@@ -0,0 +1,26 @@
+﻿using CarRentalPlatform.Domain.Common;
+using CarRentalPlatform.Domain.Exceptions;
+
+using static CarRentalPlatform.Domain.Models.ModelConstants.Common;
+
+namespace CarRentalPlatform.Domain.Models.CarAds
+{
+    public class Manufacturer : Entity<int>
+    {
+        internal Manufacturer(string name)
+        {
+            this.Validate(name);
+
+            this.Name = name;
+        }
+
+        public string Name { get; }
+
+        public void Validate(string name)
+            => Guard.ForStringLength<InvalidCarAdException>(
+                name,
+                MinNameLength,
+                MaxNameLength,
+                nameof(this.Name));
+    }
+}
