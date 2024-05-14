e2bd33de38d3f98328d7b50ca45275af2049cab1
vikto <v.atanassovv@gmail.com>
2024-05-14 13:42:37 +0300

    add Category class

---
+++ CarRentalPlatform.Domain/Models/CarAds/Category.cs
@@ -0,0 +1,38 @@
+﻿using CarRentalPlatform.Domain.Common;
+using CarRentalPlatform.Domain.Exceptions;
+
+using static CarRentalPlatform.Domain.Models.ModelConstants.Common;
+using static CarRentalPlatform.Domain.Models.ModelConstants.Category;
+
+namespace CarRentalPlatform.Domain.Models.CarAds
+{
+    public class Category : Entity<int>
+    {
+        internal Category(string name, string description)
+        {
+            this.Validate(name, description);
+
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        private void Validate(string name, string description)
+        {
+            Guard.ForStringLength<InvalidCarAdException>(
+                name,
+                MinNameLength,
+                MaxNameLength,
+                nameof(this.Name));
+
+            Guard.ForStringLength<InvalidCarAdException>(
+                description,
+                MinDescriptionLength,
+                MaxDescriptionLength,
+                nameof(this.Description));
+        }
+    }
+}
