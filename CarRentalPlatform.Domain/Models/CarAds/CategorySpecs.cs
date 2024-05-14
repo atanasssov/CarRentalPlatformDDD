8a9af3e7357ef726f21c5cf510a897b263798279
vikto <v.atanassovv@gmail.com>
2024-05-14 22:04:33 +0300

    Add category tests

---
+++ CarRentalPlatform.Domain/Models/CarAds/CategorySpecs.cs
@@ -0,0 +1,30 @@
+﻿using CarRentalPlatform.Domain.Exceptions;
+
+using FluentAssertions;
+using Xunit;
+
+namespace CarRentalPlatform.Domain.Models.CarAds
+{
+    public class CategorySpecs
+    {
+        [Fact]
+        public void ValidCategoryShouldNotThrowException()
+        {
+            // Act
+            Action act = () => new Category("Valid name", "Valid description text");
+
+            // Assert
+            act.Should().NotThrow<InvalidCarAdException>();
+        }
+
+        [Fact]
+        public void InvalidNameShouldThrowException()
+        {
+            // Act
+            Action act = () => new Category("", "Valid description text");
+
+            // Assert
+            act.Should().Throw<InvalidCarAdException>();
+        }
+    }
+}
