33b77bf2eabc2a80f1f69fc84a21d6603e976331
vikto <v.atanassovv@gmail.com>
2024-05-15 11:05:30 +0300

    add infrastructure configurations

---
+++ CarRentalPlatform.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using CarRentalPlatform.Infrastructure.Persistence;
+
+namespace CarRentalPlatform.Infrastructure
+{
+    public static class InfrastructureConfiguration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            return services.AddDbContext<CarRentalDbContext>(options => options
+                    .UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(CarRentalDbContext).Assembly.FullName)));
+        }
+    }
+}
