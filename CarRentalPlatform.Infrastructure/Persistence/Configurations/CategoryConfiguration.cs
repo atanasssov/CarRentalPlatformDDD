4c587ab1ebce19bab0eb95e03e5a73ba1ff5717e
vikto <v.atanassovv@gmail.com>
2024-05-15 10:31:57 +0300

    add category configuration

---
+++ CarRentalPlatform.Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+using CarRentalPlatform.Domain.Models.CarAds;
+
+using static CarRentalPlatform.Domain.Models.ModelConstants.Common;
+using static CarRentalPlatform.Domain.Models.ModelConstants.Category;
+
+
+namespace CarRentalPlatform.Infrastructure.Persistence.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder
+                .HasKey(c => c.Id);
+
+            builder
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(MaxNameLength);
+
+            builder
+                .Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(MaxDescriptionLength);
+        }
+    }
+}
