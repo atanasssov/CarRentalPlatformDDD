8cc0ef580dc56684b1c5f0a437e88c3f6b888e06
vikto <v.atanassovv@gmail.com>
2024-05-15 13:19:30 +0300

    add DataRepository

---
+++ CarRentalPlatform.Infrastructure/Persistence/Repositories/DataRepository.cs
@@ -0,0 +1,21 @@
+﻿using CarRentalPlatform.Application.Contracts;
+using CarRentalPlatform.Domain.Common;
+
+namespace CarRentalPlatform.Infrastructure.Persistence.Repositories
+{
+    internal class DataRepository<TEntity> : IRepository<TEntity>
+       where TEntity : class, IAggregateRoot
+    {
+        private readonly CarRentalDbContext db;
+
+        public DataRepository(CarRentalDbContext db) => this.db = db;
+
+        public IQueryable<TEntity> All() => this.db.Set<TEntity>();
+
+        public Task<int> SaveChanges(CancellationToken cancellationToken = default)
+        {
+            return this.db.SaveChangesAsync(cancellationToken);
+        }
+           
+    }
+}
