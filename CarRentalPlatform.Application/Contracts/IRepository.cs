33533be6672b29a7b52f2eaaac7eff377ae0da6c
vikto <v.atanassovv@gmail.com>
2024-05-15 13:06:07 +0300

    add IRepository

---
+++ CarRentalPlatform.Application/Contracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarRentalPlatform.Domain.Common;
+
+namespace CarRentalPlatform.Application.Contracts
+{
+    public interface IRepository<out TEntity>
+        where TEntity : IAggregateRoot
+    {
+        IQueryable<TEntity> All();
+
+        Task<int> SaveChanges(CancellationToken cancellationToken = default);
+    }
+}
