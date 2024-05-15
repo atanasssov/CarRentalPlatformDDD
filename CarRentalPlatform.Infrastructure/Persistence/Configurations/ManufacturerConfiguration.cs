8138c58a34cdc1791bd2a3034040f5902d0b55fe
vikto <v.atanassovv@gmail.com>
2024-05-15 10:50:28 +0300

    add Manufacturer configuration

---
+++ CarRentalPlatform.Infrastructure/Persistence/Configurations/ManufacturerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using CarRentalPlatform.Domain.Models.CarAds;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using static CarRentalPlatform.Domain.Models.ModelConstants.Common;
+
+
+namespace CarRentalPlatform.Infrastructure.Persistence.Configurations
+{
+    internal class ManufacturerConfiguration : IEntityTypeConfiguration<Manufacturer>
+    {
+        public void Configure(EntityTypeBuilder<Manufacturer> builder)
+        {
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(MaxNameLength);
+        }
+    }
+}
