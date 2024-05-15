77ecc06cb2293551db2c96b64d9343d1fdcfaf72
vikto <v.atanassovv@gmail.com>
2024-05-15 08:40:05 +0300

    add web project with first controller

---
+++ CarRentalPlatform.Web/Features/CarAdsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CarRentalPlatform.Domain.Models.CarAds;
+using CarRentalPlatform.Domain.Models.Dealers;
+
+
+namespace CarRentalPlatform.Web.Features
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarAdsController : ControllerBase
+    {
+        private static readonly Dealer Dealer = new Dealer("Dealer", "+12345678");
+
+        [HttpGet]
+        public IEnumerable<CarAd> Get()
+        {
+            return Dealer
+            .CarAds
+            .Where(c => c.IsAvailable);
+        }
+       
+    }
+}
