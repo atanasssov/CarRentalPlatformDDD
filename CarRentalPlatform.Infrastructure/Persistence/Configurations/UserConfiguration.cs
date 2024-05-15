de0a0871a5df9c34bc293251c0de61cccb62516c
vikto <v.atanassovv@gmail.com>
2024-05-15 21:32:53 +0300

    Add user configurations

---
+++ CarRentalPlatform.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+using CarRentalPlatform.Infrastructure.Identity;
+
+namespace CarRentalPlatform.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasOne(u => u.Dealer)
+                .WithOne()
+                .HasForeignKey<User>("DealerId")
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
