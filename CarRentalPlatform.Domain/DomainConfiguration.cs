ac65273c885797ef80fa6432bf60c0f04e0fa579
vikto <v.atanassovv@gmail.com>
2024-05-16 22:02:33 +0300

    add DomainConfiguration and its tests

---
+++ CarRentalPlatform.Domain/DomainConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using CarRentalPlatform.Domain.Factories;
+
+namespace CarRentalPlatform.Domain
+{
+    public static class DomainConfiguration
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+        {
+            return services
+                        .Scan(scan => scan
+                        .FromCallingAssembly()
+                        .AddClasses(classes => classes.AssignableTo(typeof(IFactory<>)))
+                         .AsMatchingInterface()
+                         .WithTransientLifetime());
+        }
+    }
+}
---
+++ CarRentalPlatform.Domain/DomainConfigurationSpecs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using CarRentalPlatform.Domain.Factories.CarAds;
+using CarRentalPlatform.Domain.Factories.Dealers;
+
+using Xunit;
+using FluentAssertions;
+
+namespace CarRentalPlatform.Domain
+{
+    public class DomainConfigurationSpecs
+    {
+        [Fact]
+        public void AddDomainShouldRegisterFactories()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            var services = serviceCollection
+                .AddDomain()
+                .BuildServiceProvider();
+
+            // Assert
+            services
+                .GetService<IDealerFactory>()
+                .Should()
+                .NotBeNull();
+
+            services
+                .GetService<ICarAdFactory>()
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
