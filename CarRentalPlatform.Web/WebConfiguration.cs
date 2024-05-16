16e57d8fdae9bbd19372aa8cd4bbd07f54899367
vikto <v.atanassovv@gmail.com>
2024-05-16 10:07:56 +0300

    add WebConfiguration

---
+++ CarRentalPlatform.Web/WebConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CarRentalPlatform.Web
+{
+    public static class WebConfiguration
+    {
+        public static IServiceCollection AddWebComponents(this IServiceCollection services)
+        {
+            services
+                .AddControllers()
+                .AddNewtonsoftJson();
+
+            return services;
+        }
+    }
+}
