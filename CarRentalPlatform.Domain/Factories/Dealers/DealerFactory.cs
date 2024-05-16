f44163551438560d1579260ca80550bede4d3273
vikto <v.atanassovv@gmail.com>
2024-05-16 21:38:42 +0300

    add IDealerFactory and DealerFactory

---
+++ CarRentalPlatform.Domain/Factories/Dealers/DealerFactory.cs
@@ -0,0 +1,34 @@
+﻿using CarRentalPlatform.Domain.Models.Dealers;
+
+namespace CarRentalPlatform.Domain.Factories.Dealers
+{
+    internal class DealerFactory : IDealerFactory
+    {
+        private string dealerName = default!;
+        private string dealerPhoneNumber = default!;
+
+        public IDealerFactory WithName(string name)
+        {
+            this.dealerName = name;
+            return this;
+        }
+
+        public IDealerFactory WithPhoneNumber(string phoneNumber)
+        {
+            this.dealerPhoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Dealer Build()
+        {
+            return new Dealer(this.dealerName, this.dealerPhoneNumber);
+        }
+
+        public Dealer Build(string name, string phoneNumber)
+        {
+            return this.WithName(name)
+                        .WithPhoneNumber(phoneNumber)
+                        .Build();
+        }
+    }
+}
---
+++ CarRentalPlatform.Domain/Factories/Dealers/IDealerFactory.cs
@@ -0,0 +1,11 @@
+﻿using CarRentalPlatform.Domain.Models.Dealers;
+
+namespace CarRentalPlatform.Domain.Factories.Dealers
+{
+    public interface IDealerFactory : IFactory<Dealer>
+    {
+        IDealerFactory WithName(string name);
+
+        IDealerFactory WithPhoneNumber(string phoneNumber);
+    }
+}
