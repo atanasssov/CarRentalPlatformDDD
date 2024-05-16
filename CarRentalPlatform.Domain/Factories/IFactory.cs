cd01a4acf334edd354ec37c98bbee3cca9a7ecca
vikto <v.atanassovv@gmail.com>
2024-05-16 21:25:02 +0300

    add IFactory interface

---
+++ CarRentalPlatform.Domain/Factories/IFactory.cs
@@ -0,0 +1,9 @@
+﻿using CarRentalPlatform.Domain.Common;
+
+namespace CarRentalPlatform.Domain.Factories
+{
+    public interface IFactory<out TEntity> where TEntity : IAggregateRoot
+    {
+        TEntity Build();
+    }
+}
