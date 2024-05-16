f7aef7ae75341f1468e5e4d61d1fd742551c73f2
vikto <v.atanassovv@gmail.com>
2024-05-16 09:11:57 +0300

    add ApplicationConfiguration

---
+++ CarRentalPlatform.Application/ApplicationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace CarRentalPlatform.Application
+{
+    public static class ApplicationConfiguration
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services,
+                                                             IConfiguration configuration)
+
+        {
+            return services.Configure<ApplicationSettings>(configuration.GetSection(nameof(ApplicationSettings)),options => options.BindNonPublicProperties = true);
+        }
+    }
+}
