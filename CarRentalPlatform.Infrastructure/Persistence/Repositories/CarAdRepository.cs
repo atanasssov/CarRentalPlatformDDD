a05b1b6aa96234855a189f78a0650a67883cb83b
vikto <v.atanassovv@gmail.com>
2024-05-17 18:40:59 +0300

    add CarAdRepository

---
+++ CarRentalPlatform.Infrastructure/Persistence/Repositories/CarAdRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using CarRentalPlatform.Application.Features.Identity.CarAds.Queries.Search;
+using CarRentalPlatform.Application.Features.Identity.CarAds.Queries;
+using CarRentalPlatform.Domain.Models.CarAds;
+
+namespace CarRentalPlatform.Infrastructure.Persistence.Repositories
+{
+    internal class CarAdRepository : DataRepository<CarAd>, ICarAdRepository
+    {
+        public CarAdRepository(CarRentalDbContext db)
+            : base(db)
+        {
+        }
+
+        public async Task<IEnumerable<CarAdListingModel>> GetCarAdListings(
+            string? manufacturer = default,
+            CancellationToken cancellationToken = default)
+        {
+            var query = this.AllAvailable();
+
+            if (!string.IsNullOrWhiteSpace(manufacturer))
+            {
+                query = query
+                    .Where(car => EF
+                        .Functions
+                        .Like(car.Manufacturer.Name, $"%{manufacturer}%"));
+            }
+
+            return await query
+                .Select(car => new CarAdListingModel(
+                    car.Id,
+                    car.Manufacturer.Name,
+                    car.Model,
+                    car.ImageUrl,
+                    car.Category.Name,
+                    car.PricePerDay))
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<int> Total(CancellationToken cancellationToken = default)
+        {
+            return await this.AllAvailable()
+                        .CountAsync(cancellationToken);
+        }
+
+        private IQueryable<CarAd> AllAvailable()
+        {
+            return this.All()
+                        .Where(car => car.IsAvailable);
+        }
+    }
+}
