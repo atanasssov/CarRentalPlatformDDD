1fbf79961fbace32417dec913eaef52a8b740a74
vikto <v.atanassovv@gmail.com>
2024-05-17 18:38:39 +0300

    add ICarAdRepository

---
+++ CarRentalPlatform.Application/Features/Identity/CarAds/Queries/ICarAdRepository.cs
@@ -0,0 +1,15 @@
+﻿using CarRentalPlatform.Application.Contracts;
+using CarRentalPlatform.Application.Features.Identity.CarAds.Queries.Search;
+using CarRentalPlatform.Domain.Models.CarAds;
+
+namespace CarRentalPlatform.Application.Features.Identity.CarAds.Queries
+{
+    public interface ICarAdRepository : IRepository<CarAd>
+    {
+        Task<IEnumerable<CarAdListingModel>> GetCarAdListings(
+            string? manufacturer = default,
+            CancellationToken cancellationToken = default);
+
+        Task<int> Total(CancellationToken cancellationToken = default);
+    }
+}
